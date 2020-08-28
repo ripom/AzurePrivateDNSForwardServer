8339bc16c0872bfbdd6ffce5aec46e0a43a99660
Riccardo Pomato <riccardopomato@hotmail.com>
2020-08-28 15:20:47 +0200

    Update ConfigureDNS.sh

--- ConfigureDNS.sh
+++ ConfigureDNS.sh
@@ -2,19 +2,15 @@
 
 sudo apt-get update 
 sudo apt-get install bind9 -y
-echo $'options { 
-        directory "/var/bind"; 
-        allow-query { 
-              any; 
-}; 
-        // Configure the IPs to listen on here. 
-        listen-on port 53 { any; }; 
-        listen-on-v6 { none; }; 
-        pid-file "/var/run/named/named.pid"; 
-        allow-transfer { none; }; 
-}; 
-zone "core.windows.net" { 
-       type forward; 
-       forward only; 
-       forwarders { 168.63.129.16; }; 
+echo $'options {
+        directory "/var/cache/bind";
+        allow-query { any;};
+        // Configure the IPs to listen on here.
+        listen-on { any; };
+        listen-on-v6 { none; };
+};
+zone "core.windows.net" {
+       type forward;
+       forward only;
+       forwarders { 168.63.129.16; };
 };' > /etc/bind/named.conf
