f027d90c534b9daae91ccfad2a1c740a2459cac1
Riccardo Pomato <riccardopomato@hotmail.com>
2020-08-28 12:02:37 +0200

    Update ConfigureDNS.sh

--- ConfigureDNS.sh
+++ ConfigureDNS.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sudo apt-get update && apt-get install bind9 -y
-echo $'zone "privatelink.file.core.windows.net" { \n
-    type forward; \n
-    forward only; \n
-    forwarders { 168.63.129.16; }; \n
+echo $'zone "privatelink.file.core.windows.net" { \n \
+    type forward; \n \
+    forward only; \n \
+    forwarders { 168.63.129.16; }; \n \
 };' >> /etc/bind/named.conf
