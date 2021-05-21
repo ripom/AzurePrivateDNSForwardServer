07bcd9d58f2b08f5a33900041292edeb2b230d3c
Riccardo Pomato <riccardopomato@hotmail.com>
2021-05-21 23:06:23 +0100

    Update ConfigureDNS.sh

--- ConfigureDNS.sh
+++ ConfigureDNS.sh
@@ -8,10 +8,14 @@ echo 'options {
         // Configure the IPs to listen on here.
         listen-on { any; };
         listen-on-v6 { none; };
+        forward only;
+        forwarders {  168.63.129.16; };
+        recursion yes;
 };
-zone "core.windows.net" {
-       type forward;
-       forward only;
-       forwarders { 168.63.129.16; };
-};' > /etc/bind/named.conf
+#zone "core.windows.net" {
+#       type forward;
+#       forward only;
+#       forwarders { 168.63.129.16; };
+#};
+#' > /etc/bind/named.conf
 sudo systemctl restart bind9
